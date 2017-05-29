01de28b61168bfc83053132b77d8f15acf0acea6
Adrien Vaschalde <adrien.vaschalde@gmail.com>
2017-05-29 18:13:51 +0100

    Make separator unnecessary

--- bin/parser/index.js
+++ bin/parser/index.js
@@ -1,5 +1,3 @@
-const SEPARATOR = '<!-- -->';
-
 function parseChangelogItem(item) {
   const parts = item.trim().split('\n');
   const tagName = parts[0].trim().replace(/#/g, '').trim();
@@ -15,25 +13,42 @@ function parseChangelogItem(item) {
   };
 }
 
-function isUsableItem(potentialItem) {
-  return potentialItem.split('\n').length > 1;
-}
-
-const BADLY_FORMATTED_CHANGELOG = `An item in your CHANGELOG seems to be badly
-formatted. Please check you have no trailing separator at the end of your CHANGELOG`;
-
-function checkChangelogIsCorrectlyFormatted(items) {
-  if (!items.every(isUsableItem)) throw new Error(BADLY_FORMATTED_CHANGELOG);
-}
+const BADLY_FORMATTED_CHANGELOG = `Your CHANGELOG.md seems to be badly formatted.
+Every item should start with #v1.0.0\n##Release title\n`;
 
 function parseChangelog(stdOut) {
-  const changelogItems = stdOut.split(SEPARATOR);
-  checkChangelogIsCorrectlyFormatted(changelogItems);
-  return changelogItems.map(parseChangelogItem);
+  try {
+    return getItemsFromStdOut(stdOut);
+  } catch (e) {
+    console.error(e.message);
+    throw new Error(BADLY_FORMATTED_CHANGELOG);
+  }
+}
+
+function getItemsFromStdOut(stdOut) {
+  let indexes = [];
+  let items = [];
+
+  const itemVersionRegex = /#\s?(v\d\.\d\.?\d?)/g;
+  let match;
+  while ((match = itemVersionRegex.exec(stdOut)) !== null) {
+    indexes.push({ version: match[1], index: match.index })
+  }
+
+  if (indexes.length < 1) throw new Error(BADLY_FORMATTED_CHANGELOG);
+
+  for (let i = 0; i < indexes.length; i++) {
+    if (i < indexes.length - 1) {
+      items.push(parseChangelogItem(stdOut.substring(indexes[i].index, indexes[i+1].index)));
+    } else {
+      items.push(parseChangelogItem(stdOut.substring(indexes[i].index)));
+    }
+  }
+
+  return items;
 }
 
 module.exports = {
-  SEPARATOR,
   parseChangelog,
   parseChangelogItem,
 };
